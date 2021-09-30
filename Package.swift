81f2f601b00cb6563deaa74644731dc5ed7853c8
Andreas Pfurtscheller <andreas.pfurtscheller@gmail.com>
2021-09-30 15:44:10 +0200

    Update Pillarbox to 1.0.8

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     dependencies: [
         .package(
             url: "https://github.com/aplr/Pillarbox",
-            from: "1.0.1"
+            from: "1.0.8"
         )
     ],
     targets: [
